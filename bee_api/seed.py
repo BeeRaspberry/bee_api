8b885f85fecc873da5eb5ed7c52146eb4284dc4a
erikdeirdre <pwhite@delpwhite.org>
2018-10-01 20:38:20 -0400

    update

--- bee_api/seed.py
+++ bee_api/seed.py
@@ -2,7 +2,7 @@ import sys
 import logging
 from ast import literal_eval
 from bee_api.database import db_session, Base
-from bee_api.models import Country, StateProvince, Location
+from bee_api.models import (Country, StateProvince, Location, Role, User)
 from bee_api.app import app
 
 log = logging.getLogger(__name__)
@@ -11,31 +11,36 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-def main():
 
+def main():
     log.info('Insert Country data in database')
     with open('fixtures/country.json', 'r') as file:
         data = literal_eval(file.read())
         for record in data:
-            country = Country(**record)
-            db_session.add(country)
+            db_session.add(Country(**record))
         db_session.commit()
 
     log.info('Insert State data in database')
     with open('fixtures/state_province.json', 'r') as file:
         data = literal_eval(file.read())
         for record in data:
-            province = StateProvince(**record)
-            db_session.add(province)
+            db_session.add(StateProvince(**record))
         db_session.commit()
 
     log.info('Insert Location data in database')
     with open('fixtures/location.json', 'r') as file:
         data = literal_eval(file.read())
         for record in data:
-            location = Location(**record)
-            db_session.add(location)
+            db_session.add(Location(**record))
         db_session.commit()
+
+    log.info('Insert Role data in database')
+    with open('fixtures/role.json', 'r') as file:
+        data = literal_eval(file.read())
+        for record in data:
+            db_session.add(Role(**record))
+        db_session.commit()
+
 
 if __name__ == '__main__':
     main()
