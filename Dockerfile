91cd0ef0eadd71e9d6530d9d59fe39f84b33d00e
erikdeirdre <pwhite@delpwhite.org>
2020-05-05 20:35:37 -0400

    completed switch over

--- Dockerfile
+++ Dockerfile
@@ -42,6 +42,7 @@ RUN apt-get update && apt-get install --no-install-recommends -y \
     uwsgi \
     nginx \
     supervisor \
+    curl \
     && apt-get clean \
     && rm -rf /var/lib/apt/lists/*
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,6 @@ if [[ ! -z "${DATABASE_DIR}" && ! -f "${DATABASE_DIR}/seed" &&  ! -z "${SEED}" ]
   flask seed && touch "${DATABASE_DIR}/seed"
 fi
 
-#/usr/bin/supervisord
-#flask run --host=0.0.0.0
-gunicorn --chdir app main:app -w 2 --threads 2 -b 0.0.0.0:8000
-
+/usr/bin/supervisord
+flask run --host=0.0.0.0
+#gunicorn --chdir app main:app -w 2 --threads 2 -b 0.0.0.0:8000
